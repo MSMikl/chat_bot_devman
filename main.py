ef71617d7c4a8671f7f0790f6bd70a4f92c36c61
MSMikl <michalbl4@gmail.com>
2022-05-12 18:59:00 +0300

    if_main added

--- main.py
+++ main.py
@@ -6,41 +6,47 @@ from environs import Env
 
 from tbot import BotMessage
 
-env = Env()
-env.read_env()
-token = env('DEVMAN_TOKEN')
-telegram_id = env.int('TG_ID')
-headers = {
-    'Authorization': 'Token {}'.format(token)
-}
-payload = {}
-while True:
-    try:
-        response = requests.get(
-            'https://dvmn.org/api/long_polling/',
-            headers=headers,
-            timeout=90,
-            params=payload).json()
-    except requests.exceptions.ReadTimeout:
-        print('Таймаут соединения')
-        sleep(5)
-        continue
-    except requests.exceptions.ConnectionError:
-        print('Потеряно соединение с сервером')
-        sleep(5)
-        continue
-    if response['status'] == 'found':
-        for attempt in response['new_attempts']:
-            message = 'Преподаватель проверил работу "{}"\n{}'.format(
-                attempt['lesson_title'],
-                attempt['lesson_url'],
-            )
-            if attempt['is_negative']:
-                message += '\n Пока неудачно'
-            else:
-                message += '\n Работа сдана'
-            BotMessage(telegram_id, message)
-        payload['timestamp'] = response['last_attempt_timestamp']
-    elif response['status'] == 'timeout':
-        print('Keep waiting')
-        payload['timestamp'] = response['timestamp_to_request']
+
+def main():
+    env = Env()
+    env.read_env()
+    token = env('DEVMAN_TOKEN')
+    telegram_id = env.int('TG_ID')
+    headers = {
+        'Authorization': 'Token {}'.format(token)
+    }
+    payload = {}
+    while True:
+        try:
+            response = requests.get(
+                'https://dvmn.org/api/long_polling/',
+                headers=headers,
+                timeout=90,
+                params=payload).json()
+        except requests.exceptions.ReadTimeout:
+            print('Таймаут соединения')
+            sleep(5)
+            continue
+        except requests.exceptions.ConnectionError:
+            print('Потеряно соединение с сервером')
+            sleep(5)
+            continue
+        if response['status'] == 'found':
+            for attempt in response['new_attempts']:
+                message = 'Преподаватель проверил работу "{}"\n{}'.format(
+                    attempt['lesson_title'],
+                    attempt['lesson_url'],
+                )
+                if attempt['is_negative']:
+                    message += '\n Пока неудачно'
+                else:
+                    message += '\n Работа сдана'
+                BotMessage(telegram_id, message)
+            payload['timestamp'] = response['last_attempt_timestamp']
+        elif response['status'] == 'timeout':
+            print('Keep waiting')
+            payload['timestamp'] = response['timestamp_to_request']
+
+
+if __name__ == '__main__':
+    main()
